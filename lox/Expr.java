881ce0f62351f9e442c05b242a1822b1f0398911
simgyuhwan <rbghks19@naver.com>
2024-05-11 15:46:02 +0900

    feat: AST 구현중

---
+++ lox/Expr.java
@@ -0,0 +1,18 @@
+package lox;
+
+abstract class Expr {
+
+  static class Binary extends Expr {
+
+    public Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
+  }
+}
+
