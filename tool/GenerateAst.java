4f5158b023ab48e6b29b778b101738f3b5a180f3
simgyuhwan <rbghks19@naver.com>
2024-05-12 14:29:09 +0900

    feat: 비지터 패턴(visitor pattern)과 디버깅용 프린터 구현

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -27,11 +27,13 @@ public class GenerateAst {
     String path = outputDir + "/" + baseName + ".java";
     PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
 
-    writer.println("package tool;");
+    writer.println("package lox;");
     writer.println();
     writer.println("import java.util.List;");
     writer.println();
     writer.println("abstract class " + baseName + " {");
+
+    defineVisitor(writer, baseName, types);
 
     // AST Class
     for (String type : types) {
@@ -40,8 +42,22 @@ public class GenerateAst {
       defineType(writer, baseName, className, fields);
     }
 
+    writer.println();
+    writer.println(" abstract <R> R accept(Visitor<R> visitor);");
     writer.println("}");
     writer.close();
+  }
+
+  private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+    writer.println(" interface Visitor<R> {");
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println(
+          "   R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase()
+              + ");");
+    }
+
+    writer.println(" }");
   }
 
   private static void defineType(PrintWriter writer, String baseName, String className,
@@ -57,6 +73,13 @@ public class GenerateAst {
 
       writer.println("   this." + name + " = " + name + ";");
     }
+    writer.println("   }");
+
+    // 비지터 패턴
+    writer.println();
+    writer.println("   @Override");
+    writer.println("   <R> R accept(Visitor<R> visitor) {");
+    writer.println("   return visitor.visit" + className + baseName + "(this);");
     writer.println("   }");
 
     // 필드
