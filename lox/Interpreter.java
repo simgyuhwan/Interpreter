487ef60ef6a5bb58ddd4c5bcd0a07951818c8eb5
simgyuhwan <rbghks19@naver.com>
2024-05-19 17:00:17 +0900

    feat:  Interpreter 구현중

---
+++ lox/Interpreter.java
@@ -0,0 +1,105 @@
+package lox;
+
+import lox.Expr.Binary;
+import lox.Expr.Grouping;
+import lox.Expr.Literal;
+import lox.Expr.Unary;
+
+class Interpreter implements Expr.Visitor<Object> {
+
+  @Override
+  public Object visitBinaryExpr(Binary expr) {
+    Object left = evaluate(expr.left);
+    Object right = evaluate(expr.right);
+
+    switch (expr.operator.type) {
+      case GREATER -> {
+        return (double) left > (double) right;
+      }
+      case GREATER_EQUAL -> {
+        return (double) left >= (double) right;
+      }
+      case LESS -> {
+        return (double) left < (double) right;
+      }
+      case LESS_EQUAL -> {
+        return (double) left <= (double) right;
+      }
+      case BANG_EQUAL -> {
+        return !isEqual(left, right);
+      }
+      case EQUAL_EQUAL -> {
+        return isEqual(left, right);
+      }
+      case MINUS -> {
+        return (double) left - (double) right;
+      }
+      case SLASH -> {
+        return (double) left / (double) right;
+      }
+      case STAR -> {
+        return (double) left * (double) right;
+      }
+      case PLUS -> {
+        if (left instanceof Double && right instanceof Double) {
+          return (double) left + (double) right;
+        }
+
+        if (left instanceof String && right instanceof String) {
+          return (String) left + (String) right;
+        }
+      }
+    }
+    return null;
+  }
+
+  private boolean isEqual(Object left, Object right) {
+    if (left == null && right == null) {
+      return true;
+    }
+    if (left == null) {
+      return false;
+    }
+
+    return left.equals(right);
+  }
+
+  @Override
+  public Object visitGroupingExpr(Grouping expr) {
+    return evaluate(expr.expression);
+  }
+
+  @Override
+  public Object visitLiteralExpr(Literal expr) {
+    return expr.value;
+  }
+
+  @Override
+  public Object visitUnaryExpr(Unary expr) {
+    Object right = evaluate(expr.right);
+    switch (expr.operator.type) {
+      case BANG -> {
+        return !isTruthy(right);
+      }
+      case MINUS -> {
+        return -(double) right;
+      }
+    }
+
+    return null;
+  }
+
+  private Object evaluate(Expr expr) {
+    return expr.accept(this);
+  }
+
+  private boolean isTruthy(Object object) {
+    if (object == null) {
+      return false;
+    }
+    if (object instanceof Boolean) {
+      return (boolean) object;
+    }
+    return true;
+  }
+}
