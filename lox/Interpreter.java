650e177e40ec1973f2ba3dacb1362c23e6d8c1de
simgyuhwan <rbghks19@naver.com>
2024-06-06 12:53:56 +0900

    feat:  블록 처리 완료

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -7,200 +7,224 @@ import lox.Expr.Grouping;
 import lox.Expr.Literal;
 import lox.Expr.Unary;
 import lox.Expr.Variable;
+import lox.Stmt.Block;
 import lox.Stmt.Var;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
-	private Environment environment = new Environment();
+  private Environment environment = new Environment();
 
-	void interpret(Expr expression) {
-		try {
-			Object value = evaluate(expression);
-			System.out.println(stringify(value));
-		} catch (RuntimeError error) {
-			Lox.runtimeError(error);
-		}
-	}
+  void interpret(Expr expression) {
+    try {
+      Object value = evaluate(expression);
+      System.out.println(stringify(value));
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
 
-	void interpret(List<Stmt> statements) {
-		try {
-			for (Stmt statement : statements) {
-				execute(statement);
-			}
-		} catch (RuntimeError error) {
-			Lox.runtimeError(error);
-		}
-	}
+  void interpret(List<Stmt> statements) {
+    try {
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
 
-	private void execute(Stmt stmt) {
-		stmt.accept(this);
-	}
+  private void execute(Stmt stmt) {
+    stmt.accept(this);
+  }
 
-	private String stringify(Object object) {
-		if (object == null)
-			return "nil";
+  private String stringify(Object object) {
+    if (object == null) {
+      return "nil";
+    }
 
-		if (object instanceof Double) {
-			String text = object.toString();
-			if (text.endsWith(".0")) {
-				text = text.substring(0, text.length() - 2);
-			}
-			return text;
-		}
-		return object.toString();
-	}
+    if (object instanceof Double) {
+      String text = object.toString();
+      if (text.endsWith(".0")) {
+        text = text.substring(0, text.length() - 2);
+      }
+      return text;
+    }
+    return object.toString();
+  }
 
-	@Override
-	public Object visitBinaryExpr(Binary expr) {
-		Object left = evaluate(expr.left);
-		Object right = evaluate(expr.right);
+  @Override
+  public Object visitBinaryExpr(Binary expr) {
+    Object left = evaluate(expr.left);
+    Object right = evaluate(expr.right);
 
-		switch (expr.operator.type) {
-			case GREATER -> {
-				checkNumberOperands(expr.operator, left, right);
-				return (double)left > (double)right;
-			}
-			case GREATER_EQUAL -> {
-				checkNumberOperands(expr.operator, left, right);
-				return (double)left >= (double)right;
-			}
-			case LESS -> {
-				checkNumberOperands(expr.operator, left, right);
-				return (double)left < (double)right;
-			}
-			case LESS_EQUAL -> {
-				checkNumberOperands(expr.operator, left, right);
-				return (double)left <= (double)right;
-			}
-			case BANG_EQUAL -> {
-				return !isEqual(left, right);
-			}
-			case EQUAL_EQUAL -> {
-				return isEqual(left, right);
-			}
-			case MINUS -> {
-				checkNumberOperands(expr.operator, left, right);
-				return (double)left - (double)right;
-			}
-			case SLASH -> {
-				checkNumberOperands(expr.operator, left, right);
-				return (double)left / (double)right;
-			}
-			case STAR -> {
-				checkNumberOperands(expr.operator, left, right);
-				return (double)left * (double)right;
-			}
-			case PLUS -> {
-				if (left instanceof Double && right instanceof Double) {
-					return (double)left + (double)right;
-				}
+    switch (expr.operator.type) {
+      case GREATER -> {
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left > (double) right;
+      }
+      case GREATER_EQUAL -> {
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left >= (double) right;
+      }
+      case LESS -> {
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left < (double) right;
+      }
+      case LESS_EQUAL -> {
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left <= (double) right;
+      }
+      case BANG_EQUAL -> {
+        return !isEqual(left, right);
+      }
+      case EQUAL_EQUAL -> {
+        return isEqual(left, right);
+      }
+      case MINUS -> {
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left - (double) right;
+      }
+      case SLASH -> {
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left / (double) right;
+      }
+      case STAR -> {
+        checkNumberOperands(expr.operator, left, right);
+        return (double) left * (double) right;
+      }
+      case PLUS -> {
+        if (left instanceof Double && right instanceof Double) {
+          return (double) left + (double) right;
+        }
 
-				if (left instanceof String && right instanceof String) {
-					return (String)left + (String)right;
-				}
+        if (left instanceof String && right instanceof String) {
+          return (String) left + (String) right;
+        }
 
-				throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings");
-			}
-		}
-		return null;
-	}
+        throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings");
+      }
+    }
+    return null;
+  }
 
-	private void checkNumberOperands(Token operator, Object left, Object right) {
-		if (left instanceof Double && right instanceof Double)
-			return;
-		throw new RuntimeError(operator, "Operands must be numbers");
-	}
+  private void checkNumberOperands(Token operator, Object left, Object right) {
+    if (left instanceof Double && right instanceof Double) {
+      return;
+    }
+    throw new RuntimeError(operator, "Operands must be numbers");
+  }
 
-	private boolean isEqual(Object left, Object right) {
-		if (left == null && right == null) {
-			return true;
-		}
-		if (left == null) {
-			return false;
-		}
+  private boolean isEqual(Object left, Object right) {
+    if (left == null && right == null) {
+      return true;
+    }
+    if (left == null) {
+      return false;
+    }
 
-		return left.equals(right);
-	}
+    return left.equals(right);
+  }
 
-	@Override
-	public Object visitGroupingExpr(Grouping expr) {
-		return evaluate(expr.expression);
-	}
+  @Override
+  public Object visitGroupingExpr(Grouping expr) {
+    return evaluate(expr.expression);
+  }
 
-	@Override
-	public Object visitLiteralExpr(Literal expr) {
-		return expr.value;
-	}
+  @Override
+  public Object visitLiteralExpr(Literal expr) {
+    return expr.value;
+  }
 
-	@Override
-	public Object visitUnaryExpr(Unary expr) {
-		Object right = evaluate(expr.right);
-		switch (expr.operator.type) {
-			case BANG -> {
-				return !isTruthy(right);
-			}
-			case MINUS -> {
-				checkNumberOperand(expr.operator, right);
-				return -(double)right;
-			}
-		}
+  @Override
+  public Object visitUnaryExpr(Unary expr) {
+    Object right = evaluate(expr.right);
+    switch (expr.operator.type) {
+      case BANG -> {
+        return !isTruthy(right);
+      }
+      case MINUS -> {
+        checkNumberOperand(expr.operator, right);
+        return -(double) right;
+      }
+    }
 
-		return null;
-	}
+    return null;
+  }
 
-	@Override
-	public Object visitVariableExpr(Variable expr) {
-		return environment.get(expr.name);
-	}
+  @Override
+  public Object visitVariableExpr(Variable expr) {
+    return environment.get(expr.name);
+  }
 
-	@Override
-	public Object visitAssignExpr(Assign expr) {
-		Object value = evaluate(expr.value);
-		environment.assign(expr.name, value);
-		return value;
-	}
+  @Override
+  public Object visitAssignExpr(Assign expr) {
+    Object value = evaluate(expr.value);
+    environment.assign(expr.name, value);
+    return value;
+  }
 
-	private void checkNumberOperand(Token operator, Object operand) {
-		if (operand instanceof Double)
-			return;
-		throw new RuntimeError(operator, "Operand must be a number");
-	}
+  private void checkNumberOperand(Token operator, Object operand) {
+    if (operand instanceof Double) {
+      return;
+    }
+    throw new RuntimeError(operator, "Operand must be a number");
+  }
 
-	private Object evaluate(Expr expr) {
-		return expr.accept(this);
-	}
+  private Object evaluate(Expr expr) {
+    return expr.accept(this);
+  }
 
-	private boolean isTruthy(Object object) {
-		if (object == null) {
-			return false;
-		}
-		if (object instanceof Boolean) {
-			return (boolean)object;
-		}
-		return true;
-	}
+  private boolean isTruthy(Object object) {
+    if (object == null) {
+      return false;
+    }
+    if (object instanceof Boolean) {
+      return (boolean) object;
+    }
+    return true;
+  }
 
-	@Override
-	public Void visitExpressionStmt(Stmt.Expression stmt) {
-		evaluate(stmt.expression);
-		return null;
-	}
+  @Override
+  public Void visitBlockStmt(Block stmt) {
+    executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
 
-	@Override
-	public Void visitPrintStmt(Stmt.Print stmt) {
-		Object value = evaluate(stmt.expression);
-		System.out.println(stringify(value));
-		return null;
-	}
+  private void executeBlock(List<Stmt> statements, Environment environment) {
+    Environment previous = this.environment;
 
-	@Override
-	public Void visitVarStmt(Var stmt) {
-		Object value = null;
-		if(stmt.initializer != null) {
-			value = evaluate(stmt.initializer);
-		}
+    try {
+      this.environment = environment;
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } finally {
+      this.environment = previous;
+    }
 
-		environment.define(stmt.name.lexeme, value);
-		return null;
-	}
+  }
+
+  @Override
+  public Void visitExpressionStmt(Stmt.Expression stmt) {
+    evaluate(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitPrintStmt(Stmt.Print stmt) {
+    Object value = evaluate(stmt.expression);
+    System.out.println(stringify(value));
+    return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+
+    environment.define(stmt.name.lexeme, value);
+    return null;
+  }
 }
--- lox/Parser.java
+++ lox/Parser.java
@@ -1,252 +1,293 @@
 package lox;
 
-import static lox.TokenType.*;
+import static lox.TokenType.BANG;
+import static lox.TokenType.BANG_EQUAL;
+import static lox.TokenType.EOF;
+import static lox.TokenType.EQUAL;
+import static lox.TokenType.EQUAL_EQUAL;
+import static lox.TokenType.FALSE;
+import static lox.TokenType.GREATER;
+import static lox.TokenType.GREATER_EQUAL;
+import static lox.TokenType.IDENTIFIER;
+import static lox.TokenType.LEFT_BRACE;
+import static lox.TokenType.LEFT_PAREN;
+import static lox.TokenType.LESS;
+import static lox.TokenType.LESS_EQUAL;
+import static lox.TokenType.MINUS;
+import static lox.TokenType.NIL;
+import static lox.TokenType.NUMBER;
+import static lox.TokenType.PLUS;
+import static lox.TokenType.PRINT;
+import static lox.TokenType.RIGHT_BRACE;
+import static lox.TokenType.RIGHT_PAREN;
+import static lox.TokenType.SEMICOLON;
+import static lox.TokenType.SLASH;
+import static lox.TokenType.STAR;
+import static lox.TokenType.STRING;
+import static lox.TokenType.TRUE;
+import static lox.TokenType.VAR;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class Parser {
 
-	private static class ParseError extends RuntimeException {
+  private static class ParseError extends RuntimeException {
 
-	}
+  }
 
-	//  Expr parse() {
-	//    try {
-	//      return expression();
-	//    } catch (ParseError error) {
-	//      return null;
-	//    }
-	//  }
+  //  Expr parse() {
+  //    try {
+  //      return expression();
+  //    } catch (ParseError error) {
+  //      return null;
+  //    }
+  //  }
 
-	List<Stmt> parse() {
-		List<Stmt> statements = new ArrayList<>();
-		while (!isAtEnd()) {
+  List<Stmt> parse() {
+    List<Stmt> statements = new ArrayList<>();
+    while (!isAtEnd()) {
 //			statements.add(statement());
-            statements.add(declaration());
-		}
-		return statements;
-	}
+      statements.add(declaration());
+    }
+    return statements;
+  }
 
-    private Stmt declaration() {
-        try {
-            if(match(VAR)) return varDeclaration();
-            return statement();
-        } catch (ParseError error) {
-            synchronize();
-            return null;
-        }
-
+  private Stmt declaration() {
+    try {
+      if (match(VAR)) {
+        return varDeclaration();
+      }
+      return statement();
+    } catch (ParseError error) {
+      synchronize();
+      return null;
     }
 
-    private Stmt varDeclaration() {
-        Token name = consume(IDENTIFIER, "Expect variable name.");
+  }
 
-        Expr initializer = null;
-        if(match(EQUAL)) {
-            initializer = expression();
-        }
+  private Stmt varDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect variable name.");
 
-        consume(SEMICOLON, "Expect ';' after variable declaration.");
-        return new Stmt.Var(name, initializer);
+    Expr initializer = null;
+    if (match(EQUAL)) {
+      initializer = expression();
     }
 
-    private Stmt statement() {
-		if (match(PRINT)) {
-			return printStatement();
-		}
-		return expressionStatement();
-	}
+    consume(SEMICOLON, "Expect ';' after variable declaration.");
+    return new Stmt.Var(name, initializer);
+  }
 
-	private Stmt expressionStatement() {
-		Expr expr = expression();
-		consume(SEMICOLON, "Expect ';', after, expression");
-		return new Stmt.Expression(expr);
-	}
+  private Stmt statement() {
+    if (match(PRINT)) {
+      return printStatement();
+    }
+    if (match(LEFT_BRACE)) {
+      return new Stmt.Block(block());
+    }
+    return expressionStatement();
+  }
 
-	private Stmt printStatement() {
-		Expr value = expression();
-		consume(SEMICOLON, "Expect ';' after value.");
-		return new Stmt.Print(value);
-	}
+  private List<Stmt> block() {
+    List<Stmt> statements = new ArrayList<>();
 
-	private final List<Token> tokens;
-	private int current = 0;
+    while (!check(RIGHT_BRACE) && !isAtEnd()) {
+      statements.add(declaration());
+    }
 
-	public Parser(List<Token> tokens) {
-		this.tokens = tokens;
-	}
+    consume(RIGHT_BRACE, "Expect ') after block");
+    return statements;
+  }
+
+  private Stmt expressionStatement() {
+    Expr expr = expression();
+    consume(SEMICOLON, "Expect ';', after, expression");
+    return new Stmt.Expression(expr);
+  }
+
+  private Stmt printStatement() {
+    Expr value = expression();
+    consume(SEMICOLON, "Expect ';' after value.");
+    return new Stmt.Print(value);
+  }
+
+  private final List<Token> tokens;
+  private int current = 0;
+
+  public Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
 
 
-	private Expr expression() {
-		return assignment();
-	}
+  private Expr expression() {
+    return assignment();
+  }
 
-	private Expr assignment() {
-		Expr expr = equality();
+  private Expr assignment() {
+    Expr expr = equality();
 
-		if(match(EQUAL)) {
-			Token equals = previous();
-			Expr value = assignment();
+    if (match(EQUAL)) {
+      Token equals = previous();
+      Expr value = assignment();
 
-			if(expr instanceof Expr.Variable) {
-				Token name = ((Expr.Variable)expr).name;
-				return new Expr.Assign(name, value);
-			}
-			error(equals, "Invalid assignment target.");
-		}
-		return expr;
-	}
+      if (expr instanceof Expr.Variable) {
+        Token name = ((Expr.Variable) expr).name;
+        return new Expr.Assign(name, value);
+      }
+      error(equals, "Invalid assignment target.");
+    }
+    return expr;
+  }
 
-	private Expr equality() {
-		Expr expr = comparison();
+  private Expr equality() {
+    Expr expr = comparison();
 
-		while (match(BANG_EQUAL, EQUAL_EQUAL)) {
-			Token operator = previous();
-			Expr right = comparison();
-			expr = new Expr.Binary(expr, operator, right);
-		}
+    while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+      Token operator = previous();
+      Expr right = comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
 
-		return expr;
-	}
+    return expr;
+  }
 
-	private Expr comparison() {
-		Expr expr = term();
+  private Expr comparison() {
+    Expr expr = term();
 
-		while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
-			Token operator = previous();
-			Expr right = term();
-			expr = new Expr.Binary(expr, operator, right);
-		}
+    while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+      Token operator = previous();
+      Expr right = term();
+      expr = new Expr.Binary(expr, operator, right);
+    }
 
-		return expr;
-	}
+    return expr;
+  }
 
-	private Expr term() {
+  private Expr term() {
 
-		Expr expr = factor();
-		while (match(MINUS, PLUS)) {
-			Token operator = previous();
-			Expr right = factor();
-			expr = new Expr.Binary(expr, operator, right);
-		}
+    Expr expr = factor();
+    while (match(MINUS, PLUS)) {
+      Token operator = previous();
+      Expr right = factor();
+      expr = new Expr.Binary(expr, operator, right);
+    }
 
-		return expr;
-	}
+    return expr;
+  }
 
-	private Expr factor() {
-		Expr expr = unary();
+  private Expr factor() {
+    Expr expr = unary();
 
-		while (match(SLASH, STAR)) {
-			Token operator = previous();
-			Expr right = unary();
-			expr = new Expr.Binary(expr, operator, right);
-		}
+    while (match(SLASH, STAR)) {
+      Token operator = previous();
+      Expr right = unary();
+      expr = new Expr.Binary(expr, operator, right);
+    }
 
-		return expr;
-	}
+    return expr;
+  }
 
-	private Expr unary() {
-		if (match(BANG, MINUS)) {
-			Token operator = previous();
-			Expr right = unary();
-			return new Expr.Unary(operator, right);
-		}
+  private Expr unary() {
+    if (match(BANG, MINUS)) {
+      Token operator = previous();
+      Expr right = unary();
+      return new Expr.Unary(operator, right);
+    }
 
-		return primary();
-	}
+    return primary();
+  }
 
-	private Expr primary() {
-		if (match(FALSE)) {
-			return new Expr.Literal(false);
-		}
-		if (match(TRUE)) {
-			return new Expr.Literal(true);
-		}
-		if (match(NIL)) {
-			return new Expr.Literal(null);
-		}
-		if (match(NUMBER, STRING)) {
-			return new Expr.Literal(previous().literal);
-		}
+  private Expr primary() {
+    if (match(FALSE)) {
+      return new Expr.Literal(false);
+    }
+    if (match(TRUE)) {
+      return new Expr.Literal(true);
+    }
+    if (match(NIL)) {
+      return new Expr.Literal(null);
+    }
+    if (match(NUMBER, STRING)) {
+      return new Expr.Literal(previous().literal);
+    }
 
-        if(match(IDENTIFIER)) {
-            return new Expr.Variable(previous());
+    if (match(IDENTIFIER)) {
+      return new Expr.Variable(previous());
+    }
+
+    if (match(LEFT_PAREN)) {
+      Expr expr = expression();
+      consume(RIGHT_PAREN, "Expect ')' after expression");
+      return new Expr.Grouping(expr);
+    }
+
+    throw error(peek(), "Expect expression");
+  }
+
+  private Token consume(TokenType type, String message) {
+    if (check(type)) {
+      return advance();
+    }
+
+    throw error(peek(), message);
+  }
+
+  private ParseError error(Token Token, String message) {
+    Lox.error(Token, message);
+    return new ParseError();
+  }
+
+  private void synchronize() {
+    advance();
+
+    while (!isAtEnd()) {
+      if (previous().type == SEMICOLON) {
+        return;
+      }
+
+      switch (peek().type) {
+        case CLASS, FUN, VAR, FOR, IF, WHILE, PRINT, RETURN -> {
         }
+      }
 
-		if (match(LEFT_PAREN)) {
-			Expr expr = expression();
-			consume(RIGHT_PAREN, "Expect ')' after expression");
-			return new Expr.Grouping(expr);
-		}
+      advance();
+    }
+  }
 
-		throw error(peek(), "Expect expression");
-	}
+  private boolean match(TokenType... types) {
+    for (TokenType type : types) {
+      if (check(type)) {
+        advance();
+        return true;
+      }
+    }
 
-	private Token consume(TokenType type, String message) {
-		if (check(type)) {
-			return advance();
-		}
+    return false;
+  }
 
-		throw error(peek(), message);
-	}
+  private boolean check(TokenType type) {
+    if (isAtEnd()) {
+      return false;
+    }
+    return peek().type == type;
+  }
 
-	private ParseError error(Token Token, String message) {
-		Lox.error(Token, message);
-		return new ParseError();
-	}
+  private Token advance() {
+    if (!isAtEnd()) {
+      current++;
+    }
+    return previous();
+  }
 
-	private void synchronize() {
-		advance();
+  private boolean isAtEnd() {
+    return peek().type == EOF;
+  }
 
-		while (!isAtEnd()) {
-			if (previous().type == SEMICOLON) {
-				return;
-			}
+  private Token peek() {
+    return tokens.get(current);
+  }
 
-			switch (peek().type) {
-				case CLASS, FUN, VAR, FOR, IF, WHILE, PRINT, RETURN -> {
-				}
-			}
-
-			advance();
-		}
-	}
-
-	private boolean match(TokenType... types) {
-		for (TokenType type : types) {
-			if (check(type)) {
-				advance();
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	private boolean check(TokenType type) {
-		if (isAtEnd()) {
-			return false;
-		}
-		return peek().type == type;
-	}
-
-	private Token advance() {
-		if (!isAtEnd()) {
-			current++;
-		}
-		return previous();
-	}
-
-	private boolean isAtEnd() {
-		return peek().type == EOF;
-	}
-
-	private Token peek() {
-		return tokens.get(current);
-	}
-
-	private Token previous() {
-		return tokens.get(current - 1);
-	}
+  private Token previous() {
+    return tokens.get(current - 1);
+  }
 }
