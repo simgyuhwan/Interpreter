29dbd9a9ed23089c99f6159451e11bab865af50b
simgyuhwan <rbghks19@naver.com>
2024-06-23 16:22:29 +0900

    feat : add for loop

--- lox/Parser.java
+++ lox/Parser.java
@@ -8,6 +8,7 @@ import static lox.TokenType.EOF;
 import static lox.TokenType.EQUAL;
 import static lox.TokenType.EQUAL_EQUAL;
 import static lox.TokenType.FALSE;
+import static lox.TokenType.FOR;
 import static lox.TokenType.GREATER;
 import static lox.TokenType.GREATER_EQUAL;
 import static lox.TokenType.IDENTIFIER;
@@ -33,6 +34,7 @@ import static lox.TokenType.VAR;
 import static lox.TokenType.WHILE;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -94,6 +96,9 @@ class Parser {
   }
 
   private Stmt statement() {
+    if (match(FOR)) {
+      return forStatement();
+    }
     if (match(IF)) {
       return ifStatement();
     }
@@ -107,6 +112,47 @@ class Parser {
       return new Stmt.Block(block());
     }
     return expressionStatement();
+  }
+
+  private Stmt forStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'for'.");
+
+    Stmt initializer;
+    if (match(SEMICOLON)) {
+      initializer = null;
+    } else if (match(VAR)) {
+      initializer = varDeclaration();
+    } else {
+      initializer = expressionStatement();
+    }
+
+    Expr condition = null;
+    if (!check(SEMICOLON)) {
+      condition = expression();
+    }
+    consume(SEMICOLON, "Expect ';' after loop condition");
+
+    Expr increment = null;
+    if (!check(RIGHT_PAREN)) {
+      increment = expression();
+    }
+    consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+    Stmt body = statement();
+
+    if (increment != null) {
+      body = new Stmt.Block(
+          Arrays.asList(
+              body,
+              new Stmt.Expression(increment)
+          )
+      );
+    }
+
+    if (condition == null) {
+      condition = new Expr.Literal(true);
+    }
+    body = new Stmt.While(condition, body);
+    return body;
   }
 
   private Stmt whileStatement() {
