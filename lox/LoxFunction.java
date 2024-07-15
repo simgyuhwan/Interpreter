a0f480ef5639172ff2b6e07621be36120845ca1b
gyuhwan <rbghks19@naver.com>
2024-07-15 17:00:20 +0900

    feat : 클로저 추가

--- lox/LoxFunction.java
+++ lox/LoxFunction.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
 	private final Stmt.Function declaration;
+	private final Environment closure;
 
-	public LoxFunction(Stmt.Function declaration) {
+	public LoxFunction(Stmt.Function declaration, Environment closure) {
 		this.declaration = declaration;
+		this.closure = closure;
 	}
 
 	@Override
 	public Object call(Interpreter interpreter, List<Object> arguments) {
-		Environment environment = new Environment(interpreter.globals);
+		Environment environment = new Environment(closure);
 		for (int i = 0; i < declaration.params.size(); i++) {
 			environment.define(declaration.params.get(i).lexeme, arguments.get(i));
 		}
