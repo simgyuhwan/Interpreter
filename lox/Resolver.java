b8f9dd545c8216e332bd880ea153f79db4ad8b1d
gyuhwan <rbghks19@naver.com>
2024-08-13 14:50:04 +0900

    feat: 변수 선언 리졸빙

--- lox/Resolver.java
+++ lox/Resolver.java
@@ -70,7 +70,25 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
 	@Override
 	public Void visitVarStmt(Stmt.Var stmt) {
+		declare(stmt.name);
+		if (stmt.initializer != null) {
+			resolve(stmt.initializer);
+		}
+		define(stmt.name);
 		return null;
+	}
+
+	private void define(Token name) {
+		if(scopes.isEmpty()) return;
+		scopes.peek().put(name.lexeme, true);
+	}
+
+	private void declare(Token name) {
+		if (scopes.isEmpty())
+			return;
+
+		Map<String, Boolean> scope = scopes.peek();
+		scope.put(name.lexeme, false);
 	}
 
 	@Override
