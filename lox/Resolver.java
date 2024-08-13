7133c9d9b7656ca8d145ecc054cd367d303990ea
gyuhwan <rbghks19@naver.com>
2024-08-13 15:00:34 +0900

    feat: 변수식 리졸빙

--- lox/Resolver.java
+++ lox/Resolver.java
@@ -79,7 +79,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 	}
 
 	private void define(Token name) {
-		if(scopes.isEmpty()) return;
+		if (scopes.isEmpty())
+			return;
 		scopes.peek().put(name.lexeme, true);
 	}
 
@@ -128,7 +129,21 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
 	@Override
 	public Void visitVariableExpr(Expr.Variable expr) {
+		if (!scopes.isEmpty() && Boolean.FALSE.equals(scopes.peek().get(expr.name.lexeme))) {
+			Lox.error(expr.name, "Can't read local variable in its own initializer.");
+		}
+
+		resolveLocal(expr, expr.name);
 		return null;
+	}
+
+	private void resolveLocal(Expr.Variable expr, Token name) {
+		for(int i = scopes.size()-1; i >= 0; i--) {
+			if(scopes.get(i).containsKey(name.lexeme)) {
+				interpreter.resolve(expr, scopes.size()-1-i);
+				return;
+			}
+		}
 	}
 
 	@Override
