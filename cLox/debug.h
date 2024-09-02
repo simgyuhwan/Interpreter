e495dab90a7c5629d8d2e7a16af2336a1a09ea86
gyuhwan <rbghks19@naver.com>
2024-09-02 12:26:56 +0900

    refactor: cLox오타 수정

--- cLox/debug.h
+++ cLox/debug.h
@@ -4,5 +4,6 @@
 #include "chunk.h"
 void disassembleChunk(Chunk* chunk, const char* name);
 int disassembleInstruction(Chunk* chunk, int offset);
+static int simpleInstruction(const char* name, int offset);
 
 #endif
