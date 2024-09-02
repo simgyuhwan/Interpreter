3743f7971155e58aff2b2fed7f273b84952f048c
gyuhwan <rbghks19@naver.com>
2024-09-02 18:03:18 +0900

    feat: 줄 정보

--- cLox/main.c
+++ cLox/main.c
@@ -1,17 +1,20 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include <windows.h>
 
 int main(int argc, const char* argv[]) {
   Chunk chunk;
   initChunk(&chunk);
 
   int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT);
-  writeChunk(&chunk, constant);
-  writeChunk(&chunk, OP_RETURN);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
 
   disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
+  Sleep(10000);
   return 0;
 }
