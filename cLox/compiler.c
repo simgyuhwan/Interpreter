a27192569d54b374918d72cc4e5e4d28768958f9
gyuhwan <rbghks19@naver.com>
2024-09-06 14:32:06 +0900

    clox: Scanner 토큰 생성 로직 추가

--- cLox/compiler.c
+++ cLox/compiler.c
@@ -6,4 +6,17 @@
 
 void compile(const char* source) {
   initScanner(source);
+  int line = -1;
+  for(;;) {
+    Token Token = scanToken();
+    if (Token.line != line) {
+      printf("%4d ", Token.line);
+      line = Token.line;
+    } else {
+      printf("    | ");
+    }
+    printf("%2d '%.*s'\n", Token.type, Token.length, Token.start);
+
+    if(Token.type == TOKEN_EOF) break;
+  }
 }
--- cLox/scanner.c
+++ cLox/scanner.c
@@ -3,6 +3,7 @@
 
 #include "common.h"
 #include "scanner.h"
+
 typedef struct {
   const char* start;
   const char* current;
@@ -15,4 +16,25 @@ void initScanner(const char* source) {
   Scanner.start = source;
   Scanner.current = source;
   Scanner.line = 1;
+}
+
+Token scanToken() {
+  scanner.start = scanner.current;
+
+  if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+  return errorToken("Unexpected character.");
+}
+
+static bool isAtEnd() {
+  return *scanner.current == '\n';
+}
+
+static Token makeToken(TokenType type) {
+  Token token;
+  token.type = type;
+  token.start = scanner.start;
+  token.length = (int)(scanner.current - scanner.start);
+  Token.line = scanner.line;
+  return token;
 }
